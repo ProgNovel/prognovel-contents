30085ad69310c43badd61142aa5d1878485908e4
ProgNovel <prognovel@gmail.com>
2020-05-13 16:40:32 +0800

    add preload cover

---
+++ .prognovel/utils.js
@@ -0,0 +1,18 @@
+function imageBase64Encode(arrayBuffer, type) {
+  let b64encoded = btoa(
+    [].reduce.call(
+      new Uint8Array(arrayBuffer),
+      (p, c) => {
+        return p + String.fromCharCode(c)
+      },
+      '',
+    ),
+  )
+
+  let mimetype = 'image/' + type
+  return 'data:' + mimetype + ';base64,' + b64encoded
+}
+
+module.exports = {
+  imageBase64Encode
+}
