b2c1e5fc3a3b4cd2e725d638c6f1bcf0ddac6b3f
ProgNovel <prognovel@gmail.com>
2020-05-15 17:56:43 +0800

    sort chapter correctly

--- .prognovel/books/compile-chapter-list.js
+++ .prognovel/books/compile-chapter-list.js
@@ -39,13 +39,26 @@ async function compileChapter(folder) {
     const chapterList = chapters.map(file => {
       const split = file.split('/')
       return split[split.length - 1]
-    })
+    }).sort(sortChapters)
+
     const info = yaml.safeLoad(fs.readFileSync(folder + '/info.yml', 'utf8'));
     const synopsis = md(fs.readFileSync(folder + '/synopsis.md', 'utf8'))
 
     meta = { ...info, synopsis, chapters: chapterList }
     fs.writeFileSync(folder + '/metadata.json', JSON.stringify(meta, null, 4))
   })
+}
+
+function sortChapters(a, b) {
+  a = convertToNumeric(a)
+  b = convertToNumeric(b)
+
+  return a - b
+}
+
+function convertToNumeric(chapterName) {
+  let s = chapterName.replace('chapter-', '').replace('.md', '').replace('-', '.')
+  return JSON.parse(s)
 }
 
 module.exports = compileChapterList
