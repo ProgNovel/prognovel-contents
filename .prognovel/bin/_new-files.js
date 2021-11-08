52fe3007dae2c2aa0e01d1b290c0228b82bb8e28
ProgNovel <prognovel@gmail.com>
2021-11-08 20:45:12 +0800

    update components lazy content

--- .prognovel/bin/_new-files.js
+++ .prognovel/bin/_new-files.js
@@ -124,19 +124,19 @@ jobs:
 const components = {
   "home-page": {
     "ComponentsLazy.svelte": `<script lang="ts">
-    import UpdatesModule from "components/updates-page/UpdatesModule.svelte";
-    import WebMonetizationBanner from "components/web-monetization/WebMonetizationBanner.svelte";
-    import Warning from "components/home-page/HomeWarning.svelte";
-    import ProgNovelPromo from "components/home-page/ProgNovelPromo.svelte";
-  </script>
-  
-  <div class="home-page__contained-component">
-    <Warning />
-    <WebMonetizationBanner />
-    <UpdatesModule seeAllLink="updates" />
-  </div>
-  
-  <ProgNovelPromo />
+  import UpdatesModule from "components/updates-page/UpdatesModule.svelte";
+  import WebMonetizationBanner from "components/web-monetization/WebMonetizationBanner.svelte";
+  import Warning from "components/home-page/HomeWarning.svelte";
+  import ProgNovelPromo from "components/home-page/ProgNovelPromo.svelte";
+</script>
+
+<div class="contain">
+  <Warning />
+  <WebMonetizationBanner />
+  <UpdatesModule seeAllLink="updates" />
+</div>
+
+<ProgNovelPromo />
   `,
   },
 };
